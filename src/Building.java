25d276035ae46160505945c3a3a31b24bfc3cb1d
srima karekal <srima589.karekal@gmail.com>
2016-04-27 10:53:04 +0530

    commit

---
+++ src/Building.java
@@ -0,0 +1,7 @@
+
+public class Building {
+public void calculate(int n,int w){
+	System.out.println("number of workers neede:" +(n*w));
+	
+}
+}
---
+++ src/Calculate.java
@@ -0,0 +1,11 @@
+
+public class Calculate extends Building {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int n=3;
+Calculate cl=new Calculate();
+ cl.calculate(3, 4);
+	}
+
+}
